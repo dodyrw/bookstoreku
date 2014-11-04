2272c5eb930696d39a2b86ea45297218e86cf5ed
dody <dodyrw@gmail.com>
2014-11-04 23:51:31 +0700

    Update list book to use grid

--- src/main/java/com/dodyrw/bookstoreku/pages/book/ListBook.java
+++ src/main/java/com/dodyrw/bookstoreku/pages/book/ListBook.java
@@ -3,10 +3,23 @@
  */
 package com.dodyrw.bookstoreku.pages.book;
 
+import java.util.List;
+
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
+
+import com.dodyrw.bookstoreku.entities.Book;
+
 /**
  * @author dodyrw
  *
  */
 public class ListBook {
-
+	@Inject
+	private Session session;
+	 
+	public List<Book> getBooks()
+	{
+	    return session.createCriteria(Book.class).list();
+	}
 }
