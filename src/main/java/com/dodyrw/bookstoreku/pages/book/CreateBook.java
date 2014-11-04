350bb2a3ab050c8d322323f299b502c8e7c9fcee
dody <dodyrw@gmail.com>
2014-11-04 23:46:42 +0700

    Update create book to save to database

--- src/main/java/com/dodyrw/bookstoreku/pages/book/CreateBook.java
+++ src/main/java/com/dodyrw/bookstoreku/pages/book/CreateBook.java
@@ -3,7 +3,11 @@
  */
 package com.dodyrw.bookstoreku.pages.book;
 
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
 
 import com.dodyrw.bookstoreku.entities.Book;
 
@@ -14,4 +18,18 @@ import com.dodyrw.bookstoreku.entities.Book;
 public class CreateBook {
 	@Property
 	private Book book;
+	
+    @Inject
+    private Session session;
+ 
+    @InjectPage
+    private ListBook listBook;
+ 
+    @CommitAfter
+    Object onSuccess()
+    {
+        session.persist(book);
+ 
+        return listBook;
+    }
 }
