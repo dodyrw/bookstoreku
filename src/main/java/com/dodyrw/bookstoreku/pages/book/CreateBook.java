c40ebbdcb7e0c89a17989d4e96454f97734cb0e0
dody <dodyrw@gmail.com>
2014-11-04 23:36:21 +0700

    Add create book page (simple, without db)

---
+++ src/main/java/com/dodyrw/bookstoreku/pages/book/CreateBook.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.dodyrw.bookstoreku.pages.book;
+
+import org.apache.tapestry5.annotations.Property;
+
+import com.dodyrw.bookstoreku.entities.Book;
+
+/**
+ * @author dodyrw
+ *
+ */
+public class CreateBook {
+	@Property
+	private Book book;
+}
