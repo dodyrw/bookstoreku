6199e967d7175b12386291a55ae0968fec1769e6
dody <dodyrw@gmail.com>
2014-11-05 09:38:33 +0700

    ListBook show alternative grid

--- src/main/java/com/dodyrw/bookstoreku/pages/book/ListBook.java
+++ src/main/java/com/dodyrw/bookstoreku/pages/book/ListBook.java
@@ -3,8 +3,12 @@
  */
 package com.dodyrw.bookstoreku.pages.book;
 
+import java.util.Iterator;
 import java.util.List;
 
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.grid.GridDataSource;
+import org.apache.tapestry5.hibernate.HibernateGridDataSource;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Session;
 
@@ -17,9 +21,27 @@ import com.dodyrw.bookstoreku.entities.Book;
 public class ListBook {
 	@Inject
 	private Session session;
+	
+	@Property
+    private Book currentBook;
 	 
+	// simple grid
+	
 	public List<Book> getBooks()
 	{
+		List<Book> books = session.createCriteria(Book.class).list();
+		
+//		for (Iterator<Book> i = books.iterator(); i.hasNext();) {
+//		    Book book = (Book) i.next();
+//		    System.out.println("Book: "+book.title);
+//		}
+		
 	    return session.createCriteria(Book.class).list();
 	}
+	
+	// alternative
+	
+	public GridDataSource getBooks2() {
+        return new HibernateGridDataSource(session,Book.class);
+    }
 }
