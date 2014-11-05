8591d1871d8820cfde913ae6b226226f6eca8d3e
dody <dodyrw@gmail.com>
2014-11-05 14:46:01 +0700

    Add category model and map relation with book

--- src/main/java/com/dodyrw/bookstoreku/entities/Category.java
+++ src/main/java/com/dodyrw/bookstoreku/entities/Category.java
@@ -1,43 +1,37 @@
 package com.dodyrw.bookstoreku.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//import javax.persistence.ManyToOne;
-
-
-
-
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import org.apache.tapestry5.beaneditor.NonVisual;
 import org.apache.tapestry5.beaneditor.Validate;
 import org.hibernate.annotations.CascadeType;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
+import org.hibernate.mapping.Collection;
 import org.hibernate.mapping.Set;
 
 @Entity
-public class Book implements Serializable {
+public class Category implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@NonVisual
 	public Long id;
 	 
-	@Validate("required,minLength=3")
-	public String title;
-	 
 	@Validate("required")
-	public String author;
-	 
-	@Validate("required")
-	public String publisher;
-
-	@ManyToOne
-	public Category category;
+	public String name;	 
+	
+	@OneToMany(mappedBy="category")
+	public List<Book> books;
+	
+//	@OneToMany(cascade = CascadeType.ALL)
+//	@JoinColumn(name = "developer_id")
+//	private List<Book> moreHobbies = new ArrayList<Hobby>();	
+	
+//	@OneToMany
+//    private Collection bookCollection;
 }
