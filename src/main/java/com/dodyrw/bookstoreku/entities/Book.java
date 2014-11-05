903f88634ab43d3492f3a3933e85998c18afc2e0
dody <dodyrw@gmail.com>
2014-11-05 16:59:51 +0700

    Update book model, add getter and setter

--- src/main/java/com/dodyrw/bookstoreku/entities/Book.java
+++ src/main/java/com/dodyrw/bookstoreku/entities/Book.java
@@ -40,4 +40,37 @@ public class Book implements Serializable {
 
 	@ManyToOne
 	public Category category;
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
 }
